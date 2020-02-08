c004f1ba905ae0a6ef9280212b7c5fbf699eb73e
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-08 20:45:39 +0500

    Добавлена модель данных ресторанов

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Rinat Abidullin on 08.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+}
