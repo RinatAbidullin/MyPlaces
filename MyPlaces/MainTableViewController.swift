29ca97b574b6ac8b219c20a39b2d8961f0b677ff
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-08 16:06:56 +0500

    Добавлены картинки ресторанов

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -32,6 +32,7 @@ class MainTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
         return cell
     }
 
