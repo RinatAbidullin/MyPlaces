c9b0dbdac6fcf386190768d88595b78d788dc52e
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-08 17:22:42 +0500

    Add custom cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Rinat Abidullin on 08.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlase: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}
