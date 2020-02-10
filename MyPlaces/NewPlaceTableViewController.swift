698da4f8173501a928531906367498bcdf0693eb
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-10 20:26:36 +0500

    Design for NewPlaceViewController

---
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewPlaceTableViewController.swift
+//  MyPlaces
+//
+//  Created by Rinat Abidullin on 10.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceTableViewController: UITextFieldDelegate {
+    //Скрываем клавиатуру по нажатию на Done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
