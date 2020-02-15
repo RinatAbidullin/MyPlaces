41d35788c3467aec0d885b7dfa9f7226bd2d6b72
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-15 14:48:51 +0500

    Map View Controller

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Rinat Abidullin on 15.02.2020.
+//  Copyright © 2020 Rinat Abidullin. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+}
