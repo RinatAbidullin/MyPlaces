949af7cc172da35290edd6e7c2af3f18da50e647
Rinat Abidullin <rinatabidullin@gmail.com>
2020-02-15 22:06:00 +0500

    User Location

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -99,6 +99,7 @@ class MapViewController: UIViewController {
         let alertController = UIAlertController(title: "Внимание", message: "Отсутствует доступ к геоданным", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(okAction)
+        present(alertController, animated: true)
     }
 }
 
